7ea18b5e6dd73d48537fb86d83f18e4c218dae03
pychang <pychang@nkust.edu.tw>
2025-10-23 11:43:41 +0800

    second upload

---
+++ ch7-2-2 copy.php
@@ -0,0 +1,12 @@
+<?php
+$arr = array(
+    "sid" => "C12345",
+    "name" => "Smith Leo",
+    "shape" => "Triangle",    
+    100
+);
+
+// foreach ($arr as $key => $vaule) {
+//     echo "$key => $value";
+// };
+foreach ($arr as $key => $value) echo "$key => $value <BR> ";
